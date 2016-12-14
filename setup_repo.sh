b4cfd145aa02cbf7f8891e67b06635ec0a515ec0
alqatari <alqatari@me.com>
2016-12-14 08:48:12 +0400

    changed the setup repo to correct github repo ssh url

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,7 +1,7 @@
 buster setup
 buster generate
 git init
-git remote add origin git@github.com:snd-ghost/alqatari.github.io.git
+git remote add origin git@github.com:alqatari/sndx.git
 git add -A
 git commit -m "Firs commit"
 git push origin master -f
